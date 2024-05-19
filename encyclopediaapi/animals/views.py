e22e3a204a1b6c4dcf975c04795345f67613b47c
g4mrpr0n <102053176+g4mrpr0n@users.noreply.github.com>
2024-05-19 20:19:14 +0300

    swaggy

--- encyclopediaapi/animals/views.py
+++ encyclopediaapi/animals/views.py
@@ -2,18 +2,52 @@ from rest_framework import generics
 from django.shortcuts import get_object_or_404
 from animals import models, serializers
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
 from rest_framework import generics, status
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
+authorization_header = openapi.Parameter(
+    'Authorization', 
+    openapi.IN_HEADER, 
+    description="Bearer <token>", 
+    type=openapi.TYPE_STRING,
+    required=True
+)
 
+page_param = openapi.Parameter(
+    'page', 
+    openapi.IN_QUERY, 
+    description="Page number", 
+    type=openapi.TYPE_INTEGER,
+    required=False
+)
 
 class AnimalList(generics.ListCreateAPIView):
-    
+    permission_classes = [IsAuthenticated]
     queryset = models.Animals.objects.all()
     serializer_class = serializers.PartialAnimalSerializer
 
+    @swagger_auto_schema(
+            manual_parameters=[authorization_header, page_param],
+            operation_description="Retrieve a list of animals")
+    
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_description="Create a new animal")
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
 
 class AnimalDetail(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
     queryset = models.Animals.objects.all()
     serializer_class = serializers.AnimalSerializer  # Use full serializer for detail view
+
+    @swagger_auto_schema(
+            manual_parameters=[authorization_header],
+            operation_description="Retrieve animal details")
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
--- encyclopediaapi/views.py
+++ encyclopediaapi/views.py
@@ -6,8 +6,34 @@ from rest_framework import status
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
+from drf_yasg.utils import swagger_auto_schema
 from django.core.exceptions import ValidationError
+from drf_yasg import openapi
+from django.core.paginator import Paginator
 
+
+authorization_header = openapi.Parameter(
+    'Authorization', 
+    openapi.IN_HEADER, 
+    description="Bearer <token>", 
+    type=openapi.TYPE_STRING,
+    required=True
+)
+
+@swagger_auto_schema(method='post', request_body=openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={
+        'username': openapi.Schema(type=openapi.TYPE_STRING, description='Username'),
+        'password': openapi.Schema(type=openapi.TYPE_STRING, description='Password'),
+    },
+    required=['username', 'password']
+), responses={200: openapi.Response('Successful Login', schema=openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={
+        'refresh': openapi.Schema(type=openapi.TYPE_STRING, description='JWT Refresh Token'),
+        'access': openapi.Schema(type=openapi.TYPE_STRING, description='JWT Access Token'),
+    }
+)), 401: 'Invalid Credentials'})
 @api_view(['POST'])
 def login(request):
     username = request.data.get('username')
@@ -22,11 +48,22 @@ def login(request):
     else:
         return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
-
+@swagger_auto_schema(method='post', request_body=openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={
+        'username': openapi.Schema(type=openapi.TYPE_STRING, description='Username'),
+        'password': openapi.Schema(type=openapi.TYPE_STRING, description='Password'),
+        'is_staff': openapi.Schema(type=openapi.TYPE_BOOLEAN, description='Is Staff', default=False),
+    },
+    required=['username', 'password']
+), responses={201: 'User registered successfully', 400: 'Bad Request'})
 @api_view(['POST'])
 def register(request):
+
+    
     username = request.data.get('username')
     password = request.data.get('password')
+    is_staff = request.data.get('is_staff')
 
     if not username or not password:
         return Response({'error': 'Username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
@@ -39,5 +76,5 @@ def register(request):
     if User.objects.filter(username=username).exists():
         return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
 
-    user = User.objects.create_user(username=username, password=password)
+    user = User.objects.create_user(username=username, password=password, is_staff=is_staff)
     return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
